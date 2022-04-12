251642baa30203fd5d1bfc0708d306da074e9c13
Nick Rakochy <nick.rakochy@gmail.com>
2022-04-11 22:21:31 -0500

    Testing testing

--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -13,8 +13,8 @@ var (
 	width = uint64(len(write)) + lenWidth
 )
 
-func TestStoreAppendRead(t *testing.T) {
-	f, err := ioutil.TempFile("", "store_append_read_test") 
+func TestStoreAppendRead(t *testing.T){
+	f, err := ioutil.TempFile("", "store_append_read_test")
 	require.NoError(t, err)
 	defer os.Remove(f.Name())
 	s, err := newStore(f)
@@ -24,76 +24,77 @@ func TestStoreAppendRead(t *testing.T) {
 	testReadAt(t, s)
 	s, err = newStore(f)
 	require.NoError(t, err)
-	testRead(t, s)
+	testRead(t,s)
 }
 
-func testAppend(t *testing.T, s *store) { 
+func testAppend(t *testing.T, s *store){
 	t.Helper()
 	for i := uint64(1); i < 4; i++ {
-	n, pos, err := s.Append(write)
-			require.NoError(t, err)
-			require.Equal(t, pos+n, width*i)
-	} 
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos + n, width * i)
+	}
 }
 
-func testRead(t *testing.T, s *store) { t.Helper()
+func testRead(t *testing.T, s *store){
+	t.Helper()
 	var pos uint64
 	for i := uint64(1); i < 4; i++ {
-			read, err := s.Read(pos)
-			require.NoError(t, err)
-			require.Equal(t, write, read)
-			pos += width
-	} 
+		read, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, write, read)
+		pos += width
+	}
 }
 
-func testReadAt(t *testing.T, s *store) {
+func testReadAt(t *testing.T, s *store){
 	t.Helper()
 	for i, off := uint64(1), int64(0); i < 4; i++ {
 		b := make([]byte, lenWidth)
 		n, err := s.ReadAt(b, off)
 		require.NoError(t, err)
-		require.Equal(t, lenWidth, n) 
+		require.Equal(t, lenWidth, n)
 		off += int64(n)
 		size := enc.Uint64(b)
 		b = make([]byte, size)
-		n, err = s.ReadAt(b, off) 
-		require.NoError(t, err) 
-		require.Equal(t, write, b) 
-		require.Equal(t, int(size), n) 
+
+		require.NoError(t, err)
+		require.Equal(t, write, b)
+		require.Equal(t, int(size), n)
+		
 		off += int64(n)
 	}
 }
 
-func TestStoreClose(t *testing.T) {
-	f, err := ioutil.TempFile("", "store_close_test") 
+func TestStoreClose(t *testing.T){
+	f, err := ioutil.TempFile("", "store_close_test")
 	require.NoError(t, err)
 	defer os.Remove(f.Name())
+
 	s, err := newStore(f)
 	require.NoError(t, err)
 	_, _, err = s.Append(write)
 	require.NoError(t, err)
+
 	f, beforeSize, err := openFile(f.Name())
 	require.NoError(t, err)
+
 	err = s.Close()
 	require.NoError(t, err)
+
 	_, afterSize, err := openFile(f.Name())
 	require.NoError(t, err)
 	require.True(t, afterSize > beforeSize)
 }
 
-func openFile(name string) (file *os.File, size int64, err error) { 
-  f, err := os.OpenFile(
-	name,
-	os.O_RDWR|os.O_CREATE|os.O_APPEND,
-	0644,
-  )
-  if err != nil {
-     return nil, 0, err
-  }
-  
-  fi, err := f.Stat(); if err != nil {
-	  return nil, 0, err
-  } 
-
-  return f, fi.Size(), nil
+func openFile(name string)(file *os.File, size int64, err error){
+	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if(err != nil){
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, fi.Size(),nil
 }
